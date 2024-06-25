830a2a834e281859d89edda6fbe17aba5a08331e
Junya Oiwa <0920oiwa@gmail.com>
2024-06-26 01:01:35 +0900

    [update]add api endpoints

---
+++ server/api/fuga.get.ts
@@ -0,0 +1,14 @@
+import moment from 'moment'
+import _ from 'lodash'
+
+moment.locale("ja");
+
+export default defineEventHandler(async () => {
+  const url = "https://worldtimeapi.org/api/timezone/Asia/Tokyo"
+  const res = await $fetch(url)
+  return {
+    currentDate: moment().format("YYYY年MM月DD日(ddd) hh:mm:ss"),
+    fetchResponse: res,
+    bool: _.isString(undefined)
+  }
+})
